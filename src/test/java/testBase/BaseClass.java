751e7cbb3e79dd7f9aa0181f47d0b513cda1c745
Nihal2309 <nihalmadvi2309@gmail.com>
2026-02-19 11:07:47 +0530

    Initial commit – set up OpenCart automation framework with account registration and login test cases

---
+++ src/test/java/testBase/BaseClass.java
@@ -0,0 +1,77 @@
+package testBase;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+public class BaseClass {
+    public WebDriver driver;
+    public Logger logger; //log4j2
+    public Properties p;
+
+    @BeforeClass
+    @Parameters({"os","browser"})
+    public void setup(String os, String br) throws IOException {
+        //Loading config.properties file
+        FileReader file=new FileReader(".//src//test//resources//config.properties");
+        p=new Properties();
+        p.load(file);
+
+        logger= LogManager.getLogger(this.getClass());
+
+        switch(br.toLowerCase()){
+            case "chrome" : driver=new ChromeDriver(); break;
+            case "edge" : driver=new EdgeDriver(); break;
+            case "safari" : driver=new SafariDriver(); break;
+            case "firefox" : driver=new FirefoxDriver(); break;
+            default:
+                System.out.println("enter valid browser"); return;
+        }
+
+
+
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get(p.getProperty("WebsiteURL"));
+        driver.manage().window().maximize();
+
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+    //user define method for generating random data for above input fields.
+    public String randomString(){
+        String generatedString = RandomStringUtils.randomAlphabetic(5);
+        return generatedString;
+    }
+
+    public String randomNumber(){
+        String generatedNumber = RandomStringUtils.randomNumeric(10);
+        return generatedNumber;
+    }
+
+    public String randomAlphaNumeric(){
+        String generatedString = RandomStringUtils.randomAlphabetic(3);
+        String generatedNumber = RandomStringUtils.randomNumeric(3);
+        return (generatedString+"@"+generatedNumber);
+    }
+
+}
---
+++ src/test/java/testCases/TC002_LoginTest.java
@@ -0,0 +1,43 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.MyAccountPage;
+import testBase.BaseClass;
+
+public class TC002_LoginTest extends BaseClass {
+
+    @Test
+    public void verify_login(){
+        logger.info("*** Starting TC002_LoginTest ***");
+
+        try {
+            //HomePage
+            HomePage hp = new HomePage(driver);
+            hp.clickMyAccount();
+            logger.info("Clicked on MyAccount");
+            hp.clickLogin();
+            logger.info("Clicked on Login");
+
+            //Login
+            LoginPage lp = new LoginPage(driver);
+            lp.setEmail(p.getProperty("email"));
+            lp.setPassword(p.getProperty("password"));
+            logger.info("Entered on Login details");
+            lp.clickLogin();
+            logger.info("Clicked on Login");
+
+            //MyAccount
+            MyAccountPage macc = new MyAccountPage(driver);
+            boolean targetPage = macc.isMyAccountPageExist();
+
+            Assert.assertEquals(targetPage, true, "Login Failed");
+        } catch (Exception e) {
+            Assert.fail();
+        }
+
+        logger.info("*** Finished TC002_LoginTest ***");
+    }
+}
